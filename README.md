962324d4db72965a8983b56f01d3bcef2d9f459b
Jose Sanchez <jmsanchezdiaz@gmail.com>
2021-02-22 10:27:06 +0000

    Set database connection

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # [Issue Tracker on Heroku](https://issue-dojo.herokuapp.com/)
 
+Project Issue Tracker
+
+1. Set NODE_ENV to test without quotes and set DB to your mongo connection string in .env file
+2. Complete the project in routes/api.js or by creating a handler/controller
+3. Add any security features to server.js
+4. Create all the funcional tests in tests/2-functional_tests.js
 
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,18 @@
 'use strict';
 
+var expect = require('chai').expect;
+//var MongoClient = require('mongodb');
+//var ObjectId = require('mongodb').ObjectID;
+let mongodb = require('mongodb')
+let mongoose = require('mongoose')
+
+const CONNECTION_STRING = process.env.DB;
+//let uri = 'mongodb+srv://M4v3r1ck:' + process.env.PW + '@cluster0.1cvj2.mongodb.net/issue_tracker?retryWrites=true&w=majority'
+
 module.exports = function (app) {
 
+  mongoose.connect(CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
+  
   app.route('/api/issues/:project')
   
     .get(function (req, res){
