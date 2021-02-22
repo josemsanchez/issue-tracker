f97203e1a2211cd46b4d09a51e0de1998b6d4c8a
Jose Sanchez <jmsanchezdiaz@gmail.com>
2021-02-22 11:11:00 +0000

    POST /api/issues/{projectname} with form data

--- routes/api.js
+++ routes/api.js
@@ -37,7 +37,24 @@ module.exports = function (app) {
     
     .post(function (req, res){
       let project = req.params.project;
+      let newIssue = new Issue({
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to || '',
+        status_text: req.body.status_text || '',
+        open: true,
+        created_on: new Date().toUTCString(),
+        updated_on: new Date().toUTCString(),
+        project: project
+      })
       
+      newIssue.save((error, savedIssue) => {
+        if(!error && savedIssue){
+          console.log('Saved Issue is')
+          console.log(savedIssue)
+        }
+      })
     })
     
     .put(function (req, res){
